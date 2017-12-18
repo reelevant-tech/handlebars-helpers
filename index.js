34abc898a42ccfb627d286d8a15b8422a687b8cd
Gauthier Deroo <g.deroo@gmail.com>
2017-12-18 11:18:11 +0100

    add package `handlebars-helpers` with multi helpers

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict'
 
 var Handlebars = require('handlebars')
-var registerMoment = require('./src/helpers/moment').register
+// var registerMoment = require('./src/helpers/moment').register
 var registerMinMax = require('./src/helpers/min-max').register
 var registerTimeRange = require('./src/helpers/time-range').register
 
-registerMoment(Handlebars)
+// registerMoment(Handlebars)
 registerMinMax(Handlebars)
 registerTimeRange(Handlebars)
+require('handlebars-helpers')({
+  handlebars: Handlebars
+})
 
 module.exports = Handlebars
