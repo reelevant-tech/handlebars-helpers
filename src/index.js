48eae64885cf4e0b42cc5ef54ab01ee503e1f724
Gauthier Deroo <g.deroo@gmail.com>
2017-12-19 12:01:40 +0100

    add `contains` helpers to HandleBars

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const { register: registerMinMax } = require('./helpers/min_max')
 const { register: registerMoment } = require('./helpers/moment')
 const { register: registerTimeRange } = require('./helpers/time_range')
 const { register: registerJustHelpers } = require('./helpers/just_helpers')
+const { register: registerContains } = require('./helpers/contains')
 
 registerMinMax(Handlebars)
 registerMoment(Handlebars)
 registerTimeRange(Handlebars)
 registerJustHelpers(Handlebars)
+registerContains(Handlebars)
 
 module.exports = Handlebars
