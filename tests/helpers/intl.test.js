825566dbe0446d648385e9971d592eb48239a565
Frantz Gauthier <frantz@myentropy.org>
2018-02-12 12:02:21 +0100

    update tests for `formatNumber`

--- tests/helpers/intl.test.js
+++ tests/helpers/intl.test.js
@@ -7,8 +7,10 @@ test('helpers > intl > format number', (t) => {
   var tpl = Handlebars.compile('{{formatNumber price style="currency" currency="EUR"}}')
 
   const frenchContext = { data: { intl: { locales: 'fr-FR' } } }
+  const usContext = { data: { intl: { locales: 'en-US' } } }
 
   t.equal(tpl({price: 20.1}, frenchContext), '20,10 €')
+  t.equal(tpl({price: 20.1}, usContext), '€20.10')
 
   t.end()
 })
