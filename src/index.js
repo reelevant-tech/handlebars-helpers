8a5aba435ff24c0a7bd127d9898eb923dae9f7aa
Frantz Gauthier <frantz@myentropy.org>
2018-02-13 15:38:54 +0100

    feat #2499 Format currency with `handlebars-intl` (#4)
    * add `handlebars-intl` package
    
    * feat #2499 Add `handlebars-intl` for `formatNumber` helper
    
    * update tests for `formatNumber`
    
    * chore Update to circleci v2 and set icu
    
    * chore Fix `circleci` config
    
    * chore Update `circleci` config

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const { register: registerMoment } = require('./helpers/moment')
 const { register: registerTimeRange } = require('./helpers/time_range')
 const { register: registerJustHelpers } = require('./helpers/just_helpers')
 const { register: registerContains } = require('./helpers/contains')
+const HandlebarsIntl = require('handlebars-intl')
 
 registerMinMax(Handlebars)
 registerMoment(Handlebars)
 registerTimeRange(Handlebars)
 registerJustHelpers(Handlebars)
 registerContains(Handlebars)
+HandlebarsIntl.registerWith(Handlebars)
 
 module.exports = Handlebars
