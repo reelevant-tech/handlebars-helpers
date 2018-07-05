41e71269ab97d9a9189e04dbc0a5b9118bb048cb
caloudinou <pascalou@gmail.com>
2018-07-05 12:05:32 +0200

    fix(helpers/moment.js): #RE-2915 fix bug if undefined (#6)
    * feat #2499 Format currency with `handlebars-intl` (#4)
    
    * add `handlebars-intl` package
    
    * feat #2499 Add `handlebars-intl` for `formatNumber` helper
    
    * update tests for `formatNumber`
    
    * chore Update to circleci v2 and set icu
    
    * chore Fix `circleci` config
    
    * chore Update `circleci` config
    
    * fix(helpers/moment.js): #RE-2915 fix bug if undefined
    
    * fix(test/helper/moment): modify the static past time in dynamic value

--- src/helpers/moment.js
+++ src/helpers/moment.js
@@ -4,41 +4,49 @@
  * https://raw.githubusercontent.com/helpers/handlebars-helper-moment/master/moment.js
  * duplicate because: published package is outdated (no timezone) and master was broken
  */
+const moment = require('moment-timezone')
+const _ = require('lodash')
 
 module.exports.register = function (Handlebars) {
-  var moment = require('moment-timezone')
-  var _ = require('lodash')
-
+  /**
+   * helper moment
+   * @todo : (RE-2916) check condition context and context.hash
+   * @param context {*}
+   * @param block {*}
+   * @return {string}
+   */
   Handlebars.registerHelper('moment', function (context, block) {
     if (context && context.hash) {
       block = _.cloneDeep(context)
       context = undefined
     }
 
-    var date = moment(context)
+    const date = moment(context)
+    date.tz(block.hash.tz || 'Europe/Paris')
+    date.locale(block.hash.locale || 'fr')
+    /**
+     * variable formatDate
+     * @type {[string , moment]}
+     */
+    const formatDate = Object
+      .entries(block.hash)
+      .reduce((final, current) => {
+        if (current[1] != null && current[0] !== 'format') {
+          final = final[current[0]](current[1])
+        }
+        /**
+         * match to legacy
+         */
+        if (current[0] === 'from' && current[1] == null) {
+          final = final[current[0]]()
+        }
+        return final
+      }, date)
 
-    date = date.tz(block.hash.tz || 'Europe/Paris')
-
-    var hasFormat = false
-
-    // Reset the language back to default before doing anything else
-    date.locale('fr')
-
-    for (var i in block.hash) {
-      if (i === 'format') {
-        hasFormat = true
-      } else if (date[i] && i !== 'tz') {
-        date = date[i](block.hash[i])
-      } else if (i !== 'tz') {
-        console.log('moment.js does not support "' + i + '"')
-      }
-    }
-
-    if (hasFormat) {
-      date = date.format(block.hash.format)
-    }
-
-    return date
+    /**
+     *  to ckeck and call [format] method
+     */
+    return formatDate['format'] ? formatDate['format'](block.hash.format || 'LLLL') : formatDate
   })
 
   Handlebars.registerHelper('duration', function (context, block) {
