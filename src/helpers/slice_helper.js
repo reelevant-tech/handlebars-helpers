ecf30864393dbac61a1a610588d7d18074249c09
baudic <julien.baudic@epita.fr>
2019-02-18 16:33:29 +0100

    feat(substring helper): [RE-3342] add substring helper (#11)
    * helper for substring
    
    * test
    
    * export new helper
    
    * change helper name + tests
    
    * fix cases
    
    * refacto

---
+++ src/helpers/slice_helper.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { isObject } = require('lodash')
+
+module.exports.register = function (Handlebars) {
+  /**
+   * this helper allow to get substring
+   * Handlebars set a context (Object) in params. If no param setted, context will be in str. If str is setted, context will in start ...
+   * isObject is used to check if a given param is the context or not
+   * @param {String} str - original string to process
+   * @param {Number} start - start index (included)
+   * @param {Number} stendart - end index (excluded)
+   */
+  Handlebars.registerHelper('slice', function (str, start, end) {
+    if (start != null && !isObject(start) && end != null && !isObject(end)) {
+      // {{slice 'toto tata' 1 2}}
+      return str.slice(start, end)
+    } else if (start != null && !isObject(start)) {
+      // {{slice 'toto tata' 1}}
+      return str.slice(start)
+    } else if (!isObject(str)) {
+      // {{slice 'toto tata'}}
+      return str
+    }
+    return ''
+  })
+}
---
+++ tests/helpers/slice_helper.test.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const test = require('tape')
+const Handlebars = require('handlebars')
+
+const { register: registerContains } = require('../../src/helpers/slice_helper')
+
+registerContains(Handlebars)
+
+test('helpers > slice no params', (t) => {
+  const context = { data: { intl: { locales: 'fr-FR' } } }
+  var tpl = Handlebars.compile('{{slice}}')
+  t.equal(tpl({}, context), '')
+  t.end()
+})
+
+test('helpers > slice on string', (t) => {
+  const context = { data: { intl: { locales: 'fr-FR' } } }
+  var tpl = Handlebars.compile('{{slice str}}')
+  t.equal(tpl({ str: 'first sentance' }, context), 'first sentance')
+  t.end()
+})
+
+test('helpers > slice on string with start and end', (t) => {
+  const context = { data: { intl: { locales: 'fr-FR' } } }
+  const str = 'first sentance'
+  const excpected = str.slice(2, 4)
+  var tpl = Handlebars.compile('{{slice str start end}}')
+  t.equal(tpl({ str, start: 2, end: 4 }, context), excpected)
+  t.end()
+})
+
+test('helpers > slice on string with start and end, end < start ', (t) => {
+  const context = { data: { intl: { locales: 'fr-FR' } } }
+  const str = 'first sentance'
+  const excpected = str.slice(2, 1)
+  var tpl = Handlebars.compile('{{slice str start end}}')
+  t.equal(tpl({ str, start: 2, end: 1 }, context), excpected)
+  t.end()
+})
+
+test('helpers > slice on string with start', (t) => {
+  const context = { data: { intl: { locales: 'fr-FR' } } }
+  const str = 'first sentance'
+  const excpected = str.slice(2)
+  var tpl = Handlebars.compile('{{slice str start}}')
+  t.equal(tpl({ str, start: 2 }, context), excpected)
+  t.end()
+})
