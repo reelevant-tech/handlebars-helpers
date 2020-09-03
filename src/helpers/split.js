a55336d819518e1fab1d8e09d2c1782a4bbec280
Frantz Gauthier <frantz@myentropy.org>
2020-09-03 15:30:36 +0200

    feat: [RE-4996] Pass mutiple arguments to a moment method
    - array arguments is passed spreaded to Moment method
    - modify `split` to work on mutiple string params (and as a side-effect allow to convert params to array)

--- src/helpers/split.js
+++ src/helpers/split.js
@@ -1,5 +1,10 @@
 module.exports.register = function (Handlebars) {
-  Handlebars.registerHelper('split', function (str) {
-    return str.split(';')
+  Handlebars.registerHelper('split', function (...args) {
+    return args.reduce((acc, arg) => {
+      if (typeof arg === 'string') {
+        return [...acc, ...arg.split(';')]
+      }
+      return acc
+    }, [])
   })
 }
